99765231016b1a55bb546447534e07715a38f070
atsmile <atsmile8723@gmail.com>
2026-04-29 17:53:37 +0900

    add: FaqのStory作成・型/データ管理追加

--- app/components/sections/Faq/index.tsx
+++ app/components/sections/Faq/index.tsx
@@ -1,30 +1,7 @@
 import SectionHeader from "@ui/SectionHeader";
 import SectionWrapper from "@ui/SectionWrapper";
 import FaqItem from "./FaqItem";
-
-const faqs = [
-  {
-    question: "団体でも可能ですか？",
-    answer: "はい、団体でも可能ですのでお気軽にお越しください。",
-  },
-  {
-    question: "カラオケの機種は何ですか？",
-    answer: "最新のLIVE DAMをご用意しております。",
-  },
-  {
-    question: "食べ物の持ち込みは可能ですか？",
-    answer: "飲食物の持ち込みも可能ですのでお気軽にお越しください。",
-  },
-  {
-    question: "飲み放題はありますか？",
-    answer:
-      "はい、単品メニューから飲み放題メニューをご用意しておりますので、ぜひお越しください。",
-  },
-  {
-    question: "キャッシュレス対応はしていますか？",
-    answer: "はい、ほぼ全てのキャッシュレス対応が可能です。",
-  },
-];
+import { faqs } from "@data/faq";
 
 export default function Faq() {
   return (
---
+++ app/data/faq.ts
@@ -0,0 +1,25 @@
+import type { FaqItem } from "@defs/types/faq";
+
+export const faqs: FaqItem[] = [
+  {
+    question: "団体でも可能ですか？",
+    answer: "はい、団体でも可能ですのでお気軽にお越しください。",
+  },
+  {
+    question: "カラオケの機種は何ですか？",
+    answer: "最新のLIVE DAMをご用意しております。",
+  },
+  {
+    question: "食べ物の持ち込みは可能ですか？",
+    answer: "飲食物の持ち込みも可能ですのでお気軽にお越しください。",
+  },
+  {
+    question: "飲み放題はありますか？",
+    answer:
+      "はい、単品メニューから飲み放題メニューをご用意しておりますので、ぜひお越しください。",
+  },
+  {
+    question: "キャッシュレス対応はしていますか？",
+    answer: "はい、ほぼ全てのキャッシュレス対応が可能です。",
+  },
+];
